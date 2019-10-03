9df7cd5f2776fbd07f4f688789c01d3a87da00f6
chul1ta26 <dayhany26@gmail.com>
2019-10-03 01:00:06 -0400

    exercise 3.2

--- src/main/java/CelsiusToFahrenheit.java
+++ src/main/java/CelsiusToFahrenheit.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class CelsiusToFahrenheit {
      public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+         double celsius;
+         double fahrenheit;
+
+         System.out.print("Enter Temp in Celsius: ");
+         celsius = sc.nextDouble();
+         fahrenheit = 32 + (celsius * 9/5);
+
+         System.out.printf("%.1f C = %.1f Fahrenheit \n", celsius, fahrenheit);
+
 
     }
 }
